2a57a498c56363e27ca42418aed183bbccd380b5
Pace417 <lon23horm01@mpw.ac.uk>
2025-03-10 14:54:05 +0000

    Add user signup functionality with SQLite database integration in app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template, request, hashlib
+from flask import Flask, render_template, request
+import hashlib, sqlite3
 
 app = Flask(__name__)
 h=hashlib.new("SHA256")
-
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS userInfo(
+            username VARCHAR(35) NOT NULL PRIMARY KEY,
+            password VARCHAR(20) NOT NULL,
+            joinedDate DATETIME,
+            accStatus BOOLEAN
+            )
+""")
 @app.route("/signup", methods=["GET", "POST"])
 def signUp():
     if request.method== "GET":
         return render_template("signup.html")
+    else:
+        cur.execute(""" INSERT INTO userInfo(username, password, joinedDate, accStatus)
+                        VALUES (?, ?, ?, ?)""",
+                    (name,password,GETDATE(),1))
+        con.commit()
     return "Signup succesful"
 
 
