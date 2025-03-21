633f8a5504f978a2d4ea97b3c0f00d8abe0e9d35
Pace417 <lon23horm01@mpw.ac.uk>
2025-03-21 12:01:56 +0000

    updated code

--- app.py
+++ app.py
@@ -1,42 +1,58 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import hashlib, sqlite3, datetime
 app = Flask(__name__)
-h=hashlib.new("SHA256")
+app.secret_key = "random"
 
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 cur.execute("""
             CREATE TABLE IF NOT EXISTS userInfo(
             username VARCHAR(35) NOT NULL PRIMARY KEY,
-            password VARCHAR(20) NOT NULL,
+            password VARCHAR(64) NOT NULL,
             joinedDate DATETIME default CURRENT_TIMESTAMP,
             accStatus BOOLEAN
             )
 """)
+con = sqlite3.connect("Database.db")
+cur = con.cursor()
+cur.execute(""" CREATE TABLE IF NOT EXISTS User(
+                Username VARCHAR(20) NOT NULL PRIMARY KEY,
+                Password VARCHAR(256) NOT NULL);
+            """)
+con.commit()
+con.close()
 
 @app.route("/signup", methods=["GET", "POST"])
-def signUp():
-    if request.method== "GET":
+def signup():
+    if request.method == "GET":
         return render_template("signup.html")
     else:
-        con = sqlite3.connect("database.db")
+        encoded = request.form['Password'].encode()
+        hash = hashlib.sha256(encoded).hexdigest()
+
+        con = sqlite3.connect("Database.db")
         cur = con.cursor()
-        cur.execute(""" INSERT INTO userInfo (username, password, accStatus)
-                        VALUES (?, ?, ?)""",
-                    (request.form["name"],request.form["password"],1))
+        cur.execute("INSERT INTO User (Username, Password) VALUES (?,?)",
+                        (request.form['Username'],hash))
         con.commit()
         con.close()
-    return "Signup succesful"
-
+    return "Signup Successful"
 
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
+
+
+def login():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        name = request.form["name"]
-        password = request.form["password"]
-        if password == "123" and name == "bob":
-            return 'Hello ' + name
+        encoded = request.form['Password'].encode()
+        hash = hashlib.sha256(encoded).hexdigest()
+        con = sqlite3.connect('Database.db')
+        cur = con.cursor()
+        cur.execute("SELECT * FROM User WHERE Username=? AND Password=?",
+                        (request.form['Username'],hash))
+        if len(cur.fetchall()) == 0:
+            return "Wrong username and password"
         else:
-            return "wrong password"
+            session['Username'] = request.form['Username']
+            return render_template("welcome.html")
